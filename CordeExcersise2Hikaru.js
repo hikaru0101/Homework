3c68218b757abce2563fde6949c2c326bcf4720d
hikaru0101 <113153578+hikaru0101@users.noreply.github.com>
2022-09-30 23:25:38 -0700

    Add files via upload

---
+++ CordeExcersise2Hikaru.js
@@ -0,0 +1,57 @@
+/*Question 1 →
+Print all the odd numbers from 0 → n , where n will be dynamic inside a function
+Function printOddNumbers(n) {
+// Write logic here
+}*/
+
+
+// function printOddNumbers (n){
+//     for (let i = 0; i<=n;i++){
+//         if(i%2===1){
+//             console.log(i,"is an odd number");
+//         }
+//     }
+// }
+
+// printOddNumbers (10);
+
+//*************************************************************************************************** */
+
+//Question2  Write a function that takes an array as a parameter and calculates the sum of the numbers
+// inside the array
+
+//出力されないなんで
+
+//let numlist =[1,2,3,4,5,6,7,8,9,10];
+
+// let sum = 0;
+
+// function numlistSum (numlist){
+//     for(let i = 0;i<numlist.length;i++){
+//       sum = sum + numlist[i]
+//     }
+//     return sum;
+// }
+
+// console.log(numlistSum(numlist))
+
+
+//********************************************************************************************************** */
+// Question3 Write a function that takes an array as a parameter and returns the largest number inside that
+// array
+
+// let numbersList = [ 20, 40, -10, 5, 40, 5, 50]; // 40
+
+
+// function largestNumber(array) {
+//     let max = 0;
+//     for (let i = 0; i< array.length; i++){
+//         if (array[i] > max){
+//             max = array[i];
+
+//         }
+//     }
+//         return max;
+// }
+
+// console.log(largestNumber(numbersList));
